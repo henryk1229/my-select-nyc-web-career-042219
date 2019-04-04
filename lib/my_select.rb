7e3c7c5adba15447218be7f3eab7e9fb72a8662a
Henry Koehler <henryk1229@gmail.com>
2019-04-04 17:54:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0 
   new_array = []
   while i < collection.length 
-    yield(collection[i]) == true ? new_array << yield(collection[i]) : false 
+    yield(collection[i]) == true ? new_array << collection[i] : false 
     i += 1 
   end   
   new_array 
